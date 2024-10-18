4bbae1f188de0023e12f1d45e38be31ad9f3e652
Blaine <blaine.curren@gmail.com>
2024-10-18 10:18:25 -0500

    added feature/login branch

--- loginForm/src/App.js
+++ loginForm/src/App.js
@@ -1,16 +1,10 @@
 import Login from "./components/Login/Login";
 import "./App.css";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-
-const msalInstance = new PublicClientApplication(msalConfig);
 
 function App() {
   return (
     <div className="App">
-      <MsalProvider instance={msalInstance}>
       <Login />
-      </MsalProvider>
     </div>
   );
 }
